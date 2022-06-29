c6164e54919550950c7b4729ca5b41dc3ef154df
Vladislav Surin <vlad_surin@mail.ru>
2022-06-29 16:02:23 +0300

    added script for search form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 "use strict";
+const btnSearchOpen = document.querySelector(".btn-search-open");
+const btnSearchClose = document.querySelector(".btn-close-search");
+const searchForm = document.querySelector(".search__form");
 
 const tabEls = document.querySelectorAll(".tabs__button");
 const articleLeftEls = document.querySelectorAll(".article__left");
 const articleRightEls = document.querySelectorAll(".article__right");
+
+btnSearchOpen.addEventListener("click", () => {
+  btnSearchOpen.classList.add("btn-search-open_active");
+  searchForm.classList.add("search__form_active");
+});
+
+btnSearchClose.addEventListener("click", () => {
+  btnSearchOpen.classList.remove("btn-search-open_active");
+  searchForm.classList.remove("search__form_active");
+});
 
 document.querySelector(".tabs").addEventListener("click", (event) => {
   const activeBtn = event.target;
